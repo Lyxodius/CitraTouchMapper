408a5389c5bee1a5da8428981789bd5162d09a82
Lyxodius <lyxodius@gmail.com>
2021-10-16 16:56:11 +0200

    v1.0

--- main.cpp
+++ main.cpp
@@ -1,24 +1,111 @@
 #include <windows.h>
-#include <iostream>
 #include <XInput.h>
+#include <vector>
 
-bool getState(XINPUT_STATE &state) {
+#define FRAME 16
+#define DEADZONE 0.5f
+
+bool getState(XINPUT_STATE &state, bool &lastConnected) {
     ZeroMemory(&state, sizeof(XINPUT_STATE));
     bool connected = XInputGetState(0, &state);
-    if (connected != ERROR_SUCCESS) {
-        printf("Controller is not connected.");
+    if (connected == ERROR_SUCCESS) {
+        if (!lastConnected) {
+            printf("Controller is connected!\nYou can now minimize this window and start playing.\n");
+        }
+        lastConnected = true;
+    } else {
+        lastConnected = false;
+        printf("Controller is not connected.\nPress any key to try again.\n");
     }
     return connected;
 }
 
-int main() {
-    XINPUT_STATE state;
-    while (getState(state) == ERROR_SUCCESS) {
-        float leftTrigger = (float) state.Gamepad.bLeftTrigger / 255;
-        printf("Trigger: %f\n", leftTrigger);
-        Sleep(16);
-        getState(state);
+struct Vector2 {
+    int x;
+    int y;
+
+    Vector2(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+};
+
+void clickAt(Vector2 position) {
+    SetCursorPos(position.x, position.y);
+
+    INPUT input = {0};
+
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, &input, sizeof(INPUT));
+
+    Sleep(FRAME * 2);
+
+    ZeroMemory(&input, sizeof(INPUT));
+    input.type = INPUT_MOUSE;
+    input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+struct MonitorRects {
+    std::vector<RECT> rcMonitors;
+
+    static BOOL CALLBACK MonitorEnum(HMONITOR hMon, HDC hdc, LPRECT lprcMonitor, LPARAM pData) {
+        MonitorRects *pThis = reinterpret_cast<MonitorRects *>(pData);
+        pThis->rcMonitors.push_back(*lprcMonitor);
+        return TRUE;
     }
 
-    return 0;
+    MonitorRects() {
+        EnumDisplayMonitors(0, 0, MonitorEnum, (LPARAM) this);
+    }
+};
+
+COLORREF getPixelColor(Vector2 position) {
+    HDC dc = GetDC(NULL);
+    COLORREF color = GetPixel(dc, position.x, position.y);
+    ReleaseDC(NULL, dc);
+    return color;
+}
+
+int main() {
+    MonitorRects monitors;
+    if (monitors.rcMonitors.size() < 2) {
+        printf("There is no second monitor.\n");
+        exit(1);
+    }
+
+    tagRECT secondMonitor = monitors.rcMonitors[1];
+    int x = secondMonitor.right - 397;
+
+    Vector2 positions[3] = {Vector2(x, 202), Vector2(x, 540), Vector2(x, 877)};
+    int positionIndex = 0;
+
+    bool lastConnected = false;
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "EndlessLoop"
+    while (true) {
+        XINPUT_STATE state;
+        bool pressed;
+        while (getState(state, lastConnected) == ERROR_SUCCESS) {
+            float leftTrigger = (float) state.Gamepad.bLeftTrigger / 255;
+            if (leftTrigger >= DEADZONE && !pressed) {
+                pressed = true;
+                clickAt(positions[positionIndex]);
+                SetCursorPos(secondMonitor.right, secondMonitor.bottom);
+                positionIndex++;
+                int max = 3;
+                COLORREF color = getPixelColor(Vector2(positions[2].x * 2, positions[2].y * 2));
+                if (color == 0x4d341f) max = 2;
+                if (positionIndex >= max) positionIndex = 0;
+            } else if (leftTrigger < DEADZONE) {
+                pressed = false;
+            }
+            Sleep(FRAME);
+        }
+        system("pause");
+    }
+#pragma clang diagnostic pop
 }
