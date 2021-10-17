c14cd76a484afc6cfc6aa545f7582f970e2bbbb5
Lyxodius <lyxodius@gmail.com>
2021-10-17 15:29:20 +0200

    Grapple Beam and pause game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include <XInput.h>
 #include <vector>
+#include <regex>
 
 #define FRAME 16
 #define DEADZONE 0.5f
@@ -51,22 +52,51 @@ void clickAt(Vector2 position) {
 struct MonitorRects {
     std::vector<RECT> rcMonitors;
 
-    static BOOL CALLBACK MonitorEnum(HMONITOR hMon, HDC hdc, LPRECT lprcMonitor, LPARAM pData) {
+    static BOOL CALLBACK callback(HMONITOR hMon, HDC hdc, LPRECT lprcMonitor, LPARAM pData) {
         MonitorRects *pThis = reinterpret_cast<MonitorRects *>(pData);
         pThis->rcMonitors.push_back(*lprcMonitor);
         return TRUE;
     }
 
     MonitorRects() {
-        EnumDisplayMonitors(0, 0, MonitorEnum, (LPARAM) this);
+        EnumDisplayMonitors(0, 0, callback, (LPARAM) this);
     }
 };
 
-COLORREF getPixelColor(Vector2 position) {
-    HDC dc = GetDC(NULL);
-    COLORREF color = GetPixel(dc, position.x, position.y);
-    ReleaseDC(NULL, dc);
-    return color;
+//COLORREF getPixelColor(Vector2 position) {
+//    HDC dc = GetDC(NULL);
+//    COLORREF color = GetPixel(dc, position.x, position.y);
+//    ReleaseDC(NULL, dc);
+//    return color;
+//}
+
+BOOL CALLBACK windowCallback(HWND hWnd, LPARAM lParam) {
+    const DWORD TITLE_SIZE = 1024;
+    CHAR windowTitle[TITLE_SIZE];
+
+    GetWindowText(hWnd, windowTitle, TITLE_SIZE);
+
+    int length = GetWindowTextLength(hWnd);
+    std::string title(&windowTitle[0]);
+    if (!IsWindowVisible(hWnd) || length == 0 || title == "Program Manager") {
+        return TRUE;
+    }
+
+    std::vector<std::string> &titles = *reinterpret_cast<std::vector<std::string> *>(lParam);
+    titles.push_back(title);
+
+    return TRUE;
+}
+
+HWND findWindow(std::string windowTitle) {
+    std::vector<std::string> titles;
+    EnumWindows(windowCallback, reinterpret_cast<LPARAM>(&titles));
+    for (const auto &title: titles) {
+        std::regex self_regex(windowTitle);
+        if (std::regex_search(title, self_regex)) {
+            return FindWindow(NULL, title.c_str());
+        }
+    }
 }
 
 int main() {
@@ -88,24 +118,52 @@ int main() {
 #pragma ide diagnostic ignored "EndlessLoop"
     while (true) {
         XINPUT_STATE state;
-        bool pressed;
+        bool lTriggerPressed = false;
+        bool rTriggerPressed = false;
+        bool r3Pressed = false;
         while (getState(state, lastConnected) == ERROR_SUCCESS) {
             float leftTrigger = (float) state.Gamepad.bLeftTrigger / 255;
-            if (leftTrigger >= DEADZONE && !pressed) {
-                pressed = true;
+            if (leftTrigger >= DEADZONE && !lTriggerPressed) {
+                lTriggerPressed = true;
+                positionIndex++;
+                if (positionIndex >= 2) positionIndex = 0;
                 clickAt(positions[positionIndex]);
                 SetCursorPos(secondMonitor.right, secondMonitor.bottom);
-                positionIndex++;
-                int max = 3;
-                COLORREF color = getPixelColor(Vector2(positions[2].x * 2, positions[2].y * 2));
-                if (color == 0x4d341f) max = 2;
-                if (positionIndex >= max) positionIndex = 0;
             } else if (leftTrigger < DEADZONE) {
-                pressed = false;
+                lTriggerPressed = false;
             }
+
+            float rightTrigger = (float) state.Gamepad.bRightTrigger / 255;
+            if (rightTrigger >= DEADZONE && !rTriggerPressed) {
+                rTriggerPressed = true;
+                clickAt(positions[2]);
+                SetCursorPos(secondMonitor.right, secondMonitor.bottom);
+            } else if (rightTrigger < DEADZONE && rTriggerPressed) {
+                rTriggerPressed = false;
+                clickAt(positions[positionIndex]);
+                SetCursorPos(secondMonitor.right, secondMonitor.bottom);
+            }
+
+            bool r3 = (state.Gamepad.wButtons & XINPUT_GAMEPAD_RIGHT_THUMB) != 0;
+            if (r3 && !r3Pressed) {
+                r3Pressed = true;
+
+                HWND citra = findWindow(R"(Citra\s\w+\s\d+)");
+                HWND citraGame = findWindow(R"(Citra\s\w+\s\|\sHEAD)");
+
+                SetForegroundWindow(citra);
+                SendMessage(citra, WM_KEYDOWN, VK_F4, 0);
+                SendMessage(citra, WM_KEYUP, VK_F4, 0);
+                SetForegroundWindow(citraGame);
+            } else if (!r3) {
+                r3Pressed = false;
+            }
+
             Sleep(FRAME);
         }
         system("pause");
     }
 #pragma clang diagnostic pop
 }
+
+//COLORREF color = getPixelColor(Vector2(positions[2].x * 2, positions[2].y * 2));
